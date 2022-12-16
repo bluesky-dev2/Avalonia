bluesky-dev2/Avalonia

commit 7eb95255cb1055c7d981b6ff20b86a014c9dc570

Don't mark all owned windows as dialogs.
This was preventing owner windows of non-modal dialogs from receiving key window status, meaning that input was broken in combination with embedding (Avalonia controls did work however because the routing was carried out internal to Avalonia).

native/Avalonia.Native/src/OSX/WindowImpl.mm
@@ -63,7 +63,7 @@ HRESULT WindowImpl::Show(bool activate, bool isDialog) {
     START_COM_CALL;
 
     @autoreleasepool {
-        _isDialog = isDialog || _parent != nullptr;
+        _isDialog = isDialog;
 
         WindowBaseImpl::Show(activate, isDialog);
 

commit 5af4d9508b2bd97e0fa67965cdd0022bfec4b4ef

Disable minimize button on owned windows again.
7eb95255c regressed disabling the minimize button on owned windows. Split what was previously `IsDialog` into two flags: `IsModal` and `IsOwned` and use `IsOwned` to control the disabled state of the minimize button.

native/Avalonia.Native/src/OSX/AvnWindow.mm
@@ -44,7 +44,7 @@
 
 -(bool) isDialog
 {
-    return _parent->IsDialog();
+    return _parent->IsModal();
 }
 
 -(double) getExtendedTitleBarHeight

native/Avalonia.Native/src/OSX/WindowBaseImpl.h
@@ -96,7 +96,7 @@ BEGIN_INTERFACE_MAP()
             IAvnClipboard *clipboard, IAvnDndResultCallback *cb,
             void *sourceHandle) override;
 
-    virtual bool IsDialog();
+    virtual bool IsModal();
 
     id<AvnWindowProtocol> GetWindowProtocol ();
                            

native/Avalonia.Native/src/OSX/WindowBaseImpl.mm
@@ -542,7 +542,7 @@ HRESULT WindowBaseImpl::BeginDragAndDropOperation(AvnDragDropEffects effects, Av
     return S_OK;
 }
 
-bool WindowBaseImpl::IsDialog() {
+bool WindowBaseImpl::IsModal() {
     return false;
 }
 

native/Avalonia.Native/src/OSX/WindowImpl.h
@@ -23,7 +23,7 @@ private:
     NSRect _preZoomSize;
     bool _transitioningWindowState;
     bool _isClientAreaExtended;
-    bool _isDialog;
+    bool _isModal;
     WindowImpl* _parent;
     std::list<WindowImpl*> _children;
     AvnExtendClientAreaChromeHints _extendClientHints;
@@ -91,7 +91,9 @@ BEGIN_INTERFACE_MAP()
 
     virtual HRESULT SetWindowState (AvnWindowState state) override;
 
-    virtual bool IsDialog() override;
+    virtual bool IsModal() override;
+    
+    bool IsOwned();
     
     virtual void BringToFront () override;
     

native/Avalonia.Native/src/OSX/WindowImpl.mm
@@ -63,7 +63,7 @@ HRESULT WindowImpl::Show(bool activate, bool isDialog) {
     START_COM_CALL;
 
     @autoreleasepool {
-        _isDialog = isDialog;
+        _isModal = isDialog;
 
         WindowBaseImpl::Show(activate, isDialog);
 
@@ -97,7 +97,7 @@ HRESULT WindowImpl::SetParent(IAvnWindow *parent) {
         
         _parent = cparent;
 
-        _isDialog = _parent != nullptr;
+        _isModal = _parent != nullptr;
         
         if(_parent != nullptr && Window != nullptr){
             // If one tries to show a child window with a minimized parent window, then the parent window will be
@@ -123,7 +123,7 @@ void WindowImpl::BringToFront()
     {
         if ([Window isVisible] && ![Window isMiniaturized])
         {
-            if(IsDialog())
+            if(IsModal())
             {
                 Activate();
             }
@@ -150,7 +150,7 @@ bool WindowImpl::CanBecomeKeyWindow()
 {
     for(auto iterator = _children.begin(); iterator != _children.end(); iterator++)
     {
-        if((*iterator)->IsDialog())
+        if((*iterator)->IsModal())
         {
             return false;
         }
@@ -569,8 +569,12 @@ HRESULT WindowImpl::SetWindowState(AvnWindowState state) {
     }
 }
 
-bool WindowImpl::IsDialog() {
-    return _isDialog;
+bool WindowImpl::IsModal() {
+    return _isModal;
+}
+
+bool WindowImpl::IsOwned() {
+    return _parent != nullptr;
 }
 
 NSWindowStyleMask WindowImpl::GetStyle() {
@@ -599,7 +603,7 @@ NSWindowStyleMask WindowImpl::GetStyle() {
             break;
     }
 
-    if (!IsDialog()) {
+    if (!IsOwned()) {
         s |= NSWindowStyleMaskMiniaturizable;
     }
 